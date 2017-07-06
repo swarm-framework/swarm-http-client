539c90ab4306a37a0ad01b908e4048749302634d
Dami <dami@creabox.net>
2017-07-06 22:47:59 +0200

    Initial import

---
+++ Config/build.cmake
@@ -0,0 +1,109 @@
+
+# Include sub projects
+find_dependencies(cxx-log)
+find_dependencies(swarm-commons)
+
+# Create targets
+add_library(swarm-http-client
+    Sources/swarm/http/client/HTTPClient.cxx Sources/swarm/http/client/HTTPClient.hxx
+)
+
+# Properties of targets
+
+# Add definitions for targets
+# Values:
+#   * Debug: -DSWARM_HTTP_CLIENT_DEBUG=1
+#   * Release: -DSWARM_HTTP_CLIENT_DEBUG=0
+#   * other: -DSWARM_HTTP_CLIENT_DEBUG=0
+target_compile_definitions(swarm-http-client  PUBLIC "SWARM_HTTP_CLIENT_DEBUG=$<CONFIG:Debug>")
+
+# Generate headers:
+include(GenerateExportHeader)
+generate_export_header(swarm-http-client)
+
+# Global includes. Used by all targets
+target_include_directories(
+    swarm-http-client 
+    
+    PUBLIC
+        "$<BUILD_INTERFACE:${CMAKE_CURRENT_LIST_DIR}/Sources>"
+        "$<BUILD_INTERFACE:${CMAKE_CURRENT_BINARY_DIR}>"
+        
+    PRIVATE
+        ${cxx-log_INCLUDE_DIR}
+        ${swarm-commons_INCLUDE_DIR}
+)
+
+set(config_install_dir "lib/cmake/${PROJECT_NAME}")
+set(include_install_dir "include")
+
+set(generated_dir "${CMAKE_CURRENT_BINARY_DIR}/generated")
+
+# Configuration
+set(version_config "${generated_dir}/${PROJECT_NAME}ConfigVersion.cmake")
+set(project_config "${generated_dir}/${PROJECT_NAME}Config.cmake")
+set(targets_export_name "${PROJECT_NAME}Targets")
+set(namespace "${PROJECT_NAME}::")
+
+# Include module with fuction 'write_basic_package_version_file'
+include(CMakePackageConfigHelpers)
+
+# Configure '<PROJECT-NAME>ConfigVersion.cmake'
+# Note: PROJECT_VERSION is used as a VERSION
+write_basic_package_version_file(
+    "${version_config}" COMPATIBILITY SameMajorVersion
+)
+
+# Configure '<PROJECT-NAME>Config.cmake'
+# Use variables:
+#   * targets_export_name
+#   * PROJECT_NAME
+configure_package_config_file(
+    "cmake/Config.cmake.in"
+    "${project_config}"
+    INSTALL_DESTINATION "${config_install_dir}"
+)
+
+# Targets:
+#   * <prefix>/lib/${target}.a
+#   * header location after install: <prefix>/include/*.hxx
+#   * headers can be included by C++ code `#include <project/*.hxx>`
+install(
+    TARGETS swarm-http-client
+    EXPORT "${targets_export_name}"
+    LIBRARY DESTINATION "lib"
+    ARCHIVE DESTINATION "lib"
+    RUNTIME DESTINATION "bin"
+    INCLUDES DESTINATION "${include_install_dir}"
+)
+
+# Headers:
+#   * Sources/${target}/*.hxx -> <prefix>/include/${target}/*.hxx
+install(
+    DIRECTORY "Sources/swarm"
+    DESTINATION "${include_install_dir}"
+    FILES_MATCHING PATTERN "*.[h;t]xx"
+)
+
+# Export headers:
+#   * ${CMAKE_CURRENT_BINARY_DIR}/${target}_export.h -> <prefix>/include/${target}_export.h
+install(
+    FILES
+        "${CMAKE_CURRENT_BINARY_DIR}/swarm-http-client_export.h"
+    DESTINATION "${include_install_dir}"
+)
+
+# Config
+#   * <prefix>/lib/cmake/${project}/${Target}Config.cmake
+install(
+    FILES "${project_config}" "${version_config}"
+    DESTINATION "${config_install_dir}"
+)
+
+# Config
+#   * <prefix>/lib/cmake/${project}/${Target}Targets.cmake
+install(
+    EXPORT "${targets_export_name}"
+    NAMESPACE "${namespace}"
+    DESTINATION "${config_install_dir}"
+)
---
+++ README.md
@@ -0,0 +1,12 @@
+# swarm-di
+Swarm library used to implement a simple dependency injection pattern
+
+# Build
+
+```
+git submodule init
+mkdir build
+cd build
+cmake ..
+make
+```
